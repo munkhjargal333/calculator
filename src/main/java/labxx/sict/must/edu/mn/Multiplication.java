13191ca9e1784af38da21c4c9861e4f697b10d7c
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-07 06:05:35 +0800

    javadoc in conf v2

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -5,7 +5,7 @@ public class Multiplication {
      * Multiply two integers.
      * @param a first number
      * @param b second number
-     * @return conf
+     * @return demo
      */
     public double multiply(double a, double b) {
         return a * b;
