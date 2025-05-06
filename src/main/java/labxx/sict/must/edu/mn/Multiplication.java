43cd377656194c5f369bd78f4073ddf8c6485aa6
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-07 06:01:56 +0800

    xd

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -1,19 +1,24 @@
 package labxx.sict.must.edu.mn;
 
 public class Multiplication {
-<<<<<<< HEAD
         /**
      * Multiply two integers.
      * @param a first number
      * @param b second number
      * @return conf
-=======
+     */
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
+package labxx.sict.must.edu.mn;
+
+public class Multiplication {
     /**
      * Multiply two integers.
      * @param a first number
      * @param b second number
      * @return product
->>>>>>> feature/conflict-demo
      */
     public double multiply(double a, double b) {
         return a * b;
