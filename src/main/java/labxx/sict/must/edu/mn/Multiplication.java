87c86f6565996facbf2806cc470e1974213f1650
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-07 06:02:29 +0800

    javadoc in develop2

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -11,16 +11,3 @@ public class Multiplication {
         return a * b;
     }
 }
-package labxx.sict.must.edu.mn;
-
-public class Multiplication {
-    /**
-     * Multiply two integers.
-     * @param a first number
-     * @param b second number
-     * @return product
-     */
-    public double multiply(double a, double b) {
-        return a * b;
-    }
-}
