da69a6af60af8cd29b8cf0824d15682bcf0826d1
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-09 09:21:25 +0800

    wrong version and contain coinflict

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -5,9 +5,9 @@ public class Multiplication {
      * Multiply two integers.
      * @param a first number
      * @param b second number
-     * @return demo
+     * @return error
      */
     public double multiply(double a, double b) {
-        return a + b;
+        return a / b;
     }
 }
