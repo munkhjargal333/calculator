3511e3cedbd768b579ccc8df41ddf8d286c8b723
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-09 15:32:06 +0800

    ёохх ort strategy гэж гарж ирээд бхын хэцшүүм бээ

--- src/test/java/labxx/sict/must/edu/mn/DivisionTest.java
+++ src/test/java/labxx/sict/must/edu/mn/DivisionTest.java
@@ -1,5 +1,6 @@
 package labxx.sict.must.edu.mn;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 public class DivisionTest {
     @Test
@@ -8,15 +9,15 @@ public class DivisionTest {
         assertEquals(2.0, calc.divide(4.0, 2.0), "4 / 2 нь 2-той тэнцүү байх ёстой");
     }
 
-    // @Test
-    // void testDivideNegative() {
-    //     Division calc = new Division();
-    //     assertEquals(-2.0, calc.divide(-4.0, 2.0), "-4 / 2 нь -2-той тэнцүү байх ёстой");
-    // }
-    //  @Test
-    // void testDivideByZero() {
-    //     Division calc = new Division();
-    //     assertThrows(IllegalArgumentException.class, () -> calc.divide(4.0, 0.0),
-    //     "Тэгээр хуваах нь exception өгчих байх ёстой");
-    // }
+    @Test
+    void testDivideNegative() {
+        Division calc = new Division();
+        assertEquals(-2.0, calc.divide(-4.0, 2.0), "-4 / 2 нь -2-той тэнцүү байх ёстой");
+    }
+     @Test
+    void testDivideByZero() {
+        Division calc = new Division();
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(4.0, 0.0),
+        "Тэгээр хуваах нь exception өгчих байх ёстой");
+    }
 }
