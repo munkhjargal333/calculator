8e408ba3f9e19dcb4e2bed2da8a66f7f0c8c8919
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-09 15:45:34 +0800

    alpha conflict

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -5,7 +5,7 @@ public class Multiplication {
      * Multiply two integers.
      * @param a first number
      * @param b second number
-     * @return number
+     * @return alpha
      */
     public double multiply(double a, double b) {
         return a - b;
