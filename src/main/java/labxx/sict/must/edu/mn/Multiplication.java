b9ab321ea5bb6d674ea211088cb92f4e0fc0601a
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-09 15:42:45 +0800

    need conflict

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -1,11 +1,11 @@
 package labxx.sict.must.edu.mn;
 
 public class Multiplication {
-        /**
+    /**
      * Multiply two integers.
      * @param a first number
      * @param b second number
-     * @return need conflect
+     * @return guuy
      */
     public double multiply(double a, double b) {
         return a * b;
